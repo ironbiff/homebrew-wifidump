bff49846222b341de59738ae0e77fd38355ec92e
Christoph Schmitz <schmitzchristoph@gmail.com>
2016-06-09 00:56:57 +0200

    add iperf3 dependency

--- wifidump.rb
+++ wifidump.rb
@@ -1,4 +1,5 @@
 class Wifidump < Formula
+	depends_on "iperf3"
   homepage "https://github.com/deichcode/wifidump"
   url "https://github.com/deichcode/wifidump/archive/0.1.tar.gz"
   sha256 "0569847d032f239ccdb884378714e2d49ada6d02acac29d17de942225e9d9c6c"
