c082daa623254b83100193c2980962237dc95095
Christoph Schmitz <schmitzchristoph@gmail.com>
2016-11-11 11:43:29 +0100

    update formulae to wifi dump 0.4

--- wifidump.rb
+++ wifidump.rb
@@ -2,8 +2,8 @@ class Wifidump < Formula
 	depends_on "iperf3"
   desc "wifidump - Small OS X script that dump some wifi data to check connection at university Koblenz. You need iperf3"
   homepage "https://github.com/deichcode/wifidump"
-  url "https://github.com/deichcode/wifidump/archive/0.3.tar.gz"
-  sha256 "9ff0cf0d6b46a7dd910b354278b2fe582040700faac7f3568812b05101370e1f"
+  url "https://github.com/deichcode/wifidump/archive/0.4.tar.gz"
+  sha256 "8a66e3ccca97ca351db316108b0d29110be3fe856c4d1c73685056fc97c4fc6c"
 
   def install
 	bin.install "wifidump"
